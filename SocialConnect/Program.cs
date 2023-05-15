306f6c12c693fe654728a68b002e81256ba1ec7f
Daniel Nygaard Vuust28 <67711921+DanielVuust@users.noreply.github.com>
2023-05-15 08:44:54 +0200

    Add endpoint mapper template

--- SocialConnect/Program.cs
+++ SocialConnect/Program.cs
@@ -1,3 +1,5 @@
+using SocialConnect.Endpoints;
+
 namespace SocialConnect
 {
     public class Program
@@ -25,25 +27,10 @@ namespace SocialConnect
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+            
 
-            var summaries = new[]
-            {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateTime.Now.AddDays(index),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
+            app.MapUserCommentsEndpoints();
+          
 
             app.Run();
         }
